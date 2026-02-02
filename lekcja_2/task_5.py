e55cfa07ddcd434ede09fc9f8aa4b192e743270c
sadayoshi91 <theprobb@gmail.com>
2026-02-02 20:14:08 +0100

    Lekcja_2

---
+++ lekcja_2/task_5.py
@@ -0,0 +1,4 @@
+#kreator postaci / nazwy rasy / klasa postaci
+race = input("Wybierz rasę postaci (Człowiek, Elf, Krasnolud): ") 
+character_class = input("Wybierz klasę postaci (Wojownik, Mag, Złodziej): ")
+print(f"Stworzyłeś postać: {race} {character_class}")   
